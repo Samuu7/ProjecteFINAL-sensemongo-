2e36c88636355193158f8abbaca865f0dc24f263
Samu Osuna <sosuna.dam@institutcampalans.net>
2021-12-19 20:53:33 +0100

    Versio Completa

---
+++ Projecte/Projecte/Entity/Tasca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecte.Entity
+{
+   public class Tasca
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //public string ResponsableTasca { get; set; }
+        public string Descripcio { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Data1 { get; set; }
+
+        public string Estat { get; set; }
+    }
+}
